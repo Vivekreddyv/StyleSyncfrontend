1052fbdd0a5462e4187eb4769357d1a4a014d3a1
sai <saivivek1414@gmail.com>
2023-08-13 12:36:01 +0530

    \/

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,7 +1,7 @@
 import { Link, useNavigate } from 'react-router-dom'
 import logo from '../utils/logo.png'
 import Cart from './cart'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 const Nav =()=>{
     const navigate=useNavigate()
     const handlelogout=()=>{
@@ -21,13 +21,18 @@ const Nav =()=>{
             setSize(false)
         }
     }
+    useEffect(()=>{
+        mob()
+    })
     return(
         <div>
             <div className='nav'>
             <Link to='/'>
             <div style={{display:'flex'}}>
                 <img className='logo' src={logo} alt=""></img>
-                {(localStorage.getItem("authtoken"))?
+                {!size?"":
+                <div>
+                    {(localStorage.getItem("authtoken"))?
                 <div>
                     <ul className='navlist'>
                     <Link to='/myorders'><li className='navli'>My Orders</li></Link>
@@ -35,6 +40,7 @@ const Nav =()=>{
                     </ul>
                 </div>
                 :""}
+                </div>}
             </div>
             </Link>
             {size?
@@ -53,8 +59,8 @@ const Nav =()=>{
                 <li style={{listStyle:'none',marginTop:'0.5rem'}}><Cart/></li>
             </ul>
         </div>
-            :<div>
-            <button className='mobcart'><Cart/></button>
+            :<div style={{display:'flex',alignItems:'center'}}>
+                <button className='mobcart'><Cart/></button>
                 <button onClick={handlehamburger} className='hamburger'><i class="fa-solid fa-bars"></i></button>
             </div>}
             
@@ -64,15 +70,23 @@ const Nav =()=>{
         <div className={`hamburgermenu${isopen?'':'close'}`}>
         <button className='hamburgerclose' onClick={handlehamburger}><i class="fa-solid fa-xmark"></i></button>
             <div className='hamburgermenudiv'>
-            <Link to='/categories/all'><li className='navli'>CATEGORIES</li></Link>
+            <Link to='/categories/all'><li style={{marginLeft:'7vw',fontSize:'2rem'}} className='navli'>CATEGORIES</li></Link>
                     {(!localStorage.getItem("authtoken"))?
                     <div className='loginsignupmob'>
-                        <Link to='/signup'><li className='navli'>SIGNUP</li></Link>
-                    <Link to='/login'><li className='navli'>LOGIN</li></Link>
+                        <Link to='/signup'><li style={{fontSize:'2rem'}} className='navli'>SIGNUP</li></Link>
+                    <Link to='/login'><li style={{fontSize:'2rem'}} className='navli'>LOGIN</li></Link>
                     </div>
                     :<div>
-                        <li className='navli' style={{fontSize:'1.3rem',marginTop:'0.3rem'}} onClick={handlelogout}>Log Out</li>
+                        <li className='navli' style={{fontSize:'1.3rem',marginTop:'0.3rem',fontSize:'2rem'}} onClick={handlelogout}>Log Out</li>
                         </div>}
+                        {(localStorage.getItem("authtoken"))?
+                <div>
+                    <ul className='navlistmob'>
+                    <Link to='/myorders'><li style={{fontSize:'2rem'}} className='navlimob'>My Orders</li></Link>
+                    <Link to='/myaccount'><li style={{fontSize:'2rem'}} className='navlimob'>My Account</li></Link>
+                    </ul>
+                </div>
+                :""}
             </div>
         </div>
         </div>
